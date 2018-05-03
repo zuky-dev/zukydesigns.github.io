b6fc62677978f1b8a5c9eb2d5d405422205d88e5
Zzuky <lukas.odler@gmail.com>
2018-05-03 23:23:04 +0200

    jquery and tabs coding

---
+++ scripts/tabs.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	
+	$('.tabs').click(function(){
+        var tab_id = $(this).attr('data-tab');
+        var tabname = $(this).parent().parent().attr('id');
+
+		$('#'+tabname+' .tabs').removeClass('current');
+		$('#'+tabname+' .tab-content').removeClass('current');
+
+		$(this).addClass('current');
+		$('#'+tabname+" #"+tab_id).addClass('current');
+	})
+
+})
