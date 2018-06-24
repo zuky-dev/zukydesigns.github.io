c0467eefb167fadb21ce4a53e19540405630affa
zukydesigns <lukas.odler@gmail.com>
2018-06-24 14:02:59 +0200

    menu content start plus js onload

--- js/main.js
+++ js/main.js
@@ -1,3 +1,44 @@
+//on page load check hash for content display
+$(document).ready(function(){
+    var hash = window.location.hash;
+    //if hash is set
+    if(hash !== ''){
+        //get all hashes
+        var idArray = [];
+        $('#content .conitem').each(function () {
+            var id = '#' + this.id;
+            idArray.push(id);
+        });
+
+        //the hash is in array|else delete wrong hash from history
+        if(idArray.indexOf(hash) != -1){
+            $('#content').toggleClass('active');
+            $('#content .conitem'+hash).toggleClass('active');
+        }else{
+            history.replaceState({}, document.title, $(location).attr('pathname'));
+        }
+    }
+});
+
+//profilepic on click show function
 $('#profilepic').on('click', function(){
     $(this).toggleClass('active');
 });
+
+//menu selection function
+$('.menuitem').on('click', function(){
+    var str = $(this).attr('href');
+    $('#content').toggleClass('active');
+    $('#content .conitem'+str).toggleClass('active');
+});
+
+//exit button in content function
+$('.exit').on('click', function(){
+    $('#content').toggleClass('active');
+    $('#content .conitem.active').toggleClass('active');
+    history.replaceState({}, document.title, $(location).attr('pathname'));
+});
+
+// TODO content scrollable is somewhat
+// TODO mobile ui css responsive
+// TODO loading link function
