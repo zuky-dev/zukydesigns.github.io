efcf1e11643db37931108497be8c086a700e74a1
zukydesigns <lukas.odler@gmail.com>
2018-06-25 15:12:03 +0200

    gallery near completion

--- js/main.js
+++ js/main.js
@@ -40,13 +40,26 @@ $('.exit').on('click', function(){
     history.replaceState({}, document.title, $(location).attr('pathname'));
 });
 
+//gallery animation on hover
+$('.imgishadow').on('mouseenter', function(){
+    $(this).parent().addClass('active');
+});
+$('.imgishadow').on('mouseleave', function(){
+    $(this).parent().removeClass('active');
+});
+
+//show text when hovering over img--bug scale-> on shadow of the image
+$('.imgishadow').on('mousemove', function(e){
+    var offx = e.pageX * 2 > $(window).width() ? 8.4 : 1;
+    var x = e.pageX - 50 * offx;
+    var y = e.pageY + $('#gallery').scrollTop() - 10;
+    $(this).parent().parent().find('#ginfo').css( 'position', 'absolute' ).css( 'top', y).css( 'left', x).show();
+});
+
+$('.imgishadow').on('mouseleave', function(e){
+    $(this).parent().parent().find('#ginfo').css( 'position', 'absolute' ).hide();
+});
+
+// TODO gallery maximize image in click
 // TODO mobile ui css responsive
 // TODO 404 loading
-
-
-$('.gitem').on('mousemove', function(e){
-    $(this).find('#ginfo').css( 'position', 'absolute' ).css( 'top', e.pageY - 10).css( 'left', e.pageX - 40).show();
-});
-$('.gitem').on('mouseleave', function(e){
-    $(this).find('#ginfo').css( 'position', 'absolute' ).hide();
-});
